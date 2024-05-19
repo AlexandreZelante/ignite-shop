331e261dfb21c32b956c8d49c9e79e615264d8f2
Alexandre Zelante <alexandre.zelante@outlook.com>
2024-05-19 18:15:27 -0300

    feat: homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,30 @@
-import Head from "next/head";
 import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import { styled } from "../styles";
+import { HomeContainer, Product } from "../styles/pages/home";
 
-const inter = Inter({ subsets: ["latin"] });
+import camiseta1 from '../assets/camisetas/1.png'
+
 
 export default function Home() {
   return (
-    <h1>Hello World!</h1>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+      <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
